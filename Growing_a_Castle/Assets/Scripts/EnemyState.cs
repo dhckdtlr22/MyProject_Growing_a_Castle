f507392cea7d62fc92c1d28cd460abbb08a3631c
Gomae <osc040825@naver.com>
2022-11-12 02:44:53 +0900

    프로토타이핑

--- Growing_a_Castle/Assets/Scripts/EnemyState.cs
+++ Growing_a_Castle/Assets/Scripts/EnemyState.cs
@@ -6,10 +6,13 @@ public class EnemyState : MonoBehaviour
 {
     public float Speed;
     public bool Attack = false;
+    public int Example_Hp;
+    public AttackZone attackZone;
     // Start is called before the first frame update
     void Start()
     {
-        
+        attackZone = GameObject.Find("AttackZone").GetComponent<AttackZone>();
+        Example_Hp = 100;
     }
 
     // Update is called once per frame
@@ -19,7 +22,11 @@ public class EnemyState : MonoBehaviour
         {
             transform.Translate(0, 0, -Speed * Time.deltaTime);
         }
-        
+        if(Example_Hp <= 0)
+        {
+            attackZone.Enemy.RemoveAt(0);
+            Destroy(gameObject);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
--- Growing_a_Castle/Assets/Scripts/UnitScript.cs
+++ Growing_a_Castle/Assets/Scripts/UnitScript.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class UnitScript : MonoBehaviour
 {
     public AttackZone attackZone;
+    public EnemyState enemyState;
+    public int Example_Damage;
+    public float Attackcur;
+    public float Attackcool;
     // Start is called before the first frame update
     void Start()
     {
         attackZone = GameObject.Find("AttackZone").GetComponent<AttackZone>();
-
+        Example_Damage = 10;
     }
 
     // Update is called once per frame
@@ -19,7 +23,17 @@ public class UnitScript : MonoBehaviour
         {
             Debug.Log(attackZone.Enemy);
             transform.LookAt(attackZone.Enemy[0].transform);
-        }    
-
+            enemyState = attackZone.Enemy[0].GetComponent<EnemyState>();
+        }
+        if(attackZone.EnemyIn == true)
+        {
+            Attackcur += Time.deltaTime;
+            if (Attackcur > Attackcool)
+            {
+                enemyState.Example_Hp -= Example_Damage;
+                Attackcur = 0;
+            }
+        }
+        
     }
 }
